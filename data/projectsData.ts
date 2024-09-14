b7ce3aa99cad31789b9cb9c11515beb6e970468f
김민재 <kimminje7267@naver.com>
2024-09-14 17:20:59 +0900

    feat: project에 react-hook-scroll 추가, project 순서 변경

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,12 +7,6 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Humming',
-    description: `당신이 흥얼거리고 있는 노래가 기억나지 않나요? 허밍을 통해 쉽게 찾아보세요. 바로 노래를 들을 수 있습니다.`,
-    imgSrc: '/static/images/humming.png',
-    href: 'https://humming.vercel.app/',
-  },
-  {
     title: 'Pickple',
     description: `같이 농구할 사람을 찾고계신가요? 픽플에서 쉽게 찾아보세요. 잘 맞는 사람들끼리 크루를 결성할수도 있습니다.`,
     imgSrc: '/static/images/pickple.png',
@@ -23,6 +17,19 @@ const projectsData: Project[] = [
     description: `니르바나는 지속적인 자극적 컨텐츠 노출로 인해 생긴 현대인들의 정신적 피로를 명상과 공유를 통해 치유하자는 목적을 가지고 만들어졌습니다.`,
     imgSrc: '/static/images/nirvana.png',
     href: 'https://nirvana-gidong.netlify.app/',
+  },
+  {
+    title: 'Humming',
+    description: `당신이 흥얼거리고 있는 노래가 기억나지 않나요? 허밍을 통해 쉽게 찾아보세요. 바로 노래를 들을 수 있습니다.`,
+    imgSrc: '/static/images/humming.png',
+    href: 'https://humming.vercel.app/',
+  },
+  {
+    title: 'React-Hook-Scroll',
+    description:
+      'React 프로젝트에서 사용할 수 있는 scroll UI 오픈소스 라이브러리입니다. NPM에 배포되어 있습니다.',
+    imgSrc: '/static/images/react-hook-scroll.png',
+    href: 'https://www.npmjs.com/package/react-hook-scroll',
   },
 ]
 
